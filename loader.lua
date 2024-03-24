0284bc04dfc19b7e580d94a4d78900a9bdf968ce
capi <89797002+nxtcapi@users.noreply.github.com>
2024-03-24 19:25:26 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,21 @@
 
+repeat wait() until game.Players.LocalPlayer.Character
+repeat wait() until game:IsLoaded() wait()
+
+
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+end)
+
+local games = {
+    [{1554960397}] = "https://raw.githubusercontent.com/SkiddyScripts/IceWare/main/Games/Car-Dealership-Tycoon/Main.lua", -- Car Dealership Tycoon
+}
+
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        wait()
+        loadstring(game:HttpGet(url))()
+        warn(game.PlaceId)
+        break
+    end
+end
